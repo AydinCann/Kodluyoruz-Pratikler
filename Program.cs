5309dcf5a6cc20c94086ab6ffaa501027f5976e4
AydinCann <cannok41@hotmail.com>
2022-01-15 18:43:50 +0300

    HelloWord

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Consol_Programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("ismiizi giriniz");
+            string name =Console.ReadLine();
+            System.Console.WriteLine("soyisminizi giriniz");
+            string surename =Console.ReadLine();
+            System.Console.WriteLine(name+" "+surename);
+            Console.ReadLine();
+
+        }
+    }
+}
